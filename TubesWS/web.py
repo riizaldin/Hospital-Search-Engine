8c8ca693d4f65b1f82b51b279d9497d4e0e639e0
Hanzel Oclihar Tjiam <117634551+hanzel77@users.noreply.github.com>
2023-12-05 23:43:50 +0700

    Add files via upload

--- TubesWS/web.py
+++ TubesWS/web.py
@@ -1,13 +1,38 @@
 from flask import Flask, render_template, redirect, request
+from math import radians, sin, cos, sqrt, atan2
 from datetime import datetime
 from SPARQLWrapper import SPARQLWrapper, JSON
 import requests
+from operator import itemgetter
+from geopy.geocoders import Nominatim
+from flask import jsonify
+from flask_cors import CORS
+
+
+def haversine_distance(lat1, lon1, lat2, lon2):
+    # Convert latitude and longitude from degrees to radians
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+    # Haversine formula
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = 6371 * c  # Radius of Earth in kilometers
+    return distance
 
 sparql = SPARQLWrapper(
     "http://localhost:3030/sumutsehat/query"
 )
 
 app = Flask(__name__)
+@app.route('/receive_location', methods=['POST'])
+def receive_location():
+    data = request.get_json()
+    latitude = data.get('latitude')
+    longitude = data.get('longitude')
+    return latitude, longitude
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -19,7 +44,7 @@ def search():
         return redirect('/')
     sparqlQuery = f"""
     PREFIX schema: <http://websemantikkelompok6.org/>
-    SELECT ?subject ?nama ?alamat ?kelas ?luas_bangunan ?luas_tanah ?blu ?jenis ?telepon ?image_url (GROUP_CONCAT(?pelayanan; separator=',') AS ?listPelayanan)
+    SELECT ?subject ?nama ?alamat ?kelas ?luas_bangunan ?kota ?luas_tanah ?blu ?jenis ?telepon ?image_url (GROUP_CONCAT(?pelayanan; separator=',') AS ?listPelayanan)
     WHERE {{
         ?subject schema:name ?nama .
         OPTIONAL {{ ?subject schema:alamat ?alamat . }}
@@ -30,10 +55,11 @@ def search():
         OPTIONAL {{ ?subject schema:jenis ?jenis . }}
         OPTIONAL {{ ?subject schema:pelayanan ?pelayanan . }}
         OPTIONAL {{ ?subject schema:telepon ?telepon . }}
+        OPTIONAL {{ ?subject schema:kota ?kota . }}
         OPTIONAL {{ ?subject schema:image_url ?image_url . }}
         FILTER (CONTAINS(LCASE(?nama), LCASE("{search_query}")) || CONTAINS(LCASE(?alamat), LCASE("{search_query}")) || CONTAINS(LCASE(?pelayanan), LCASE("{search_query}")))
     }}
-    GROUP BY ?subject ?nama ?alamat ?kelas ?luas_bangunan ?luas_tanah ?blu ?jenis ?telepon ?image_url
+    GROUP BY ?subject ?nama ?alamat ?kelas ?luas_bangunan ?luas_tanah ?blu ?jenis ?telepon ?image_url ?kota
     HAVING (BOUND(?nama))
     """
     sparql.setQuery(sparqlQuery)
@@ -189,6 +215,181 @@ def about(kota):
         tanggal = None
     return render_template("kota.html", wikiResult=wikiResult, latitude=latitude, longitude=longitude, tanggal=tanggal)
 
+@app.route('/tes/<hospital>')
+def tes(hospital):
+    sparqlQuery = f"""
+    PREFIX schema: <http://websemantikkelompok6.org/>
+    SELECT ?subject ?nama ?koordinat ?alamat  ?telepon ?image_url
+    WHERE {{
+        ?subject schema:name ?nama .
+        OPTIONAL {{ ?subject schema:alamat ?alamat . }}
+        OPTIONAL {{ ?subject schema:telepon ?telepon . }}
+        OPTIONAL {{ ?subject schema:image_url ?image_url . }}
+        ?subject schema:koordinat ?koordinat .
+        FILTER (CONTAINS(LCASE(?nama), LCASE("{hospital}")) || CONTAINS(LCASE(?alamat), LCASE("{hospital}")) || CONTAINS(LCASE(?pelayanan), LCASE("{hospital}")))
+    }}
+    
+    GROUP BY ?subject ?alamat ?telepon ?image_url ?koordinat ?nama
+    """
+    sparql.setQuery(sparqlQuery)
+    sparql.setReturnFormat(JSON)
+    ret = sparql.queryAndConvert()
+    result = ret["results"]["bindings"]
+    if result[0]['koordinat'] and result[0]['koordinat'].get('value'):
+        koordinat = result[0]['koordinat']['value']
+        koordinat = koordinat.split(',')
+        latitude = koordinat[0]
+        longitude = koordinat[1]
+    else:
+        latitude = None
+        longitude = None
+    
+    allQuery = f"""
+    PREFIX schema: <http://websemantikkelompok6.org/>
+    SELECT ?subject ?koordinat ?nama ?alamat ?telepon ?image_url
+    WHERE {{
+        ?subject schema:name ?nama .
+        OPTIONAL {{ ?subject schema:alamat ?alamat . }}
+        OPTIONAL {{ ?subject schema:telepon ?telepon . }}
+        OPTIONAL {{ ?subject schema:image_url ?image_url . }}
+        ?subject schema:koordinat ?koordinat .
+    }}
+    GROUP BY ?subject ?alamat ?telepon ?image_url ?koordinat ?nama
+    HAVING (BOUND(?nama))
+    """
+
+    sparql.setQuery(allQuery)
+    sparql.setReturnFormat(JSON)
+    all = sparql.queryAndConvert()
+    all_hospital = all["results"]["bindings"]
+
+    coordinates_dict = {}
+
+    for data in all_hospital:
+        if data.get('koordinat') and data['koordinat'].get('value'):
+            koordinat = data['koordinat']['value']
+            koordinat = koordinat.split(',')
+            latitude = koordinat[0]
+            longitude = koordinat[1]
+            
+            if data.get('nama') and data['nama'].get('value'):
+                name = data['nama']['value']
+                coordinates_dict[name] = [latitude, longitude]
+
+    count = len(coordinates_dict)
+
+    if result[0]['koordinat'] and result[0]['koordinat'].get('value'):
+        koordinat = result[0]['koordinat']['value']
+        latitude_1, longitude_1 = map(float, koordinat.split(','))
+    else:
+        latitude_1 = None
+        longitude_1 = None
+
+    # Calculate distances and store in a dictionary along with names using Haversine formula
+    distances_dict = {}
+    for name, coords in coordinates_dict.items():
+        lat2, lon2 = map(float, coords)
+        distance = haversine_distance(latitude_1, longitude_1, lat2, lon2)
+        distances_dict[name] = distance
+
+    sorted_distances = sorted(distances_dict.items(), key=itemgetter(1))[:4]
+
+    # Extract the names of the three closest hospitals
+    closest_hospitals = [name for name, _ in sorted_distances]
+
+    # Fetch details for the three closest hospitals
+    closest_hospitals_details = []
+    for name, distance in sorted_distances:
+        if name != hospital:  # Replace 'current_hospital_name' with the name of the current hospital
+            for data in all_hospital:
+                if data.get('nama') and data['nama']['value'] == name:
+                    hospital_details = {
+                        'dat': data,  # Assuming 'dat' holds the entire hospital details
+                        'distance': distance
+                    }
+                    closest_hospitals_details.append(hospital_details)
+                    break  # Stop searching once the hospital details are found
+
+        if len(closest_hospitals_details) == 3:  # Stop fetching once three hospitals are found
+            break
+
+    return render_template("tes.html", result=result, count=count, latitude=latitude, longitude=longitude, coordinates=coordinates_dict, distances=distances_dict, closest_hospitals_details=closest_hospitals_details)
+
+latitude_now = 0
+longitude_now = 0
+@app.route('/getLocation', methods=["POST"])
+def get_location():
+    global latitude_now, longitude_now
+
+    data = request.get_json()
+    latitude = data.get('latitude')
+    longitude = data.get('longitude')
+    latitude_now = latitude
+    longitude_now = longitude
+
+    # Call a function to calculate distances and retrieve closest hospitals
+    closest_hospitals_details = get_closest_hospitals(latitude, longitude)
+
+    return jsonify(closest_hospitals_details)
+
+def get_all_hospitals():
+    allQuery = f"""
+    PREFIX schema: <http://websemantikkelompok6.org/>
+    SELECT ?subject ?koordinat ?nama ?alamat ?telepon ?image_url
+    WHERE {{
+        ?subject schema:name ?nama .
+        OPTIONAL {{ ?subject schema:alamat ?alamat . }}
+        OPTIONAL {{ ?subject schema:telepon ?telepon . }}
+        OPTIONAL {{ ?subject schema:image_url ?image_url . }}
+        ?subject schema:koordinat ?koordinat .
+    }}
+    GROUP BY ?subject ?alamat ?telepon ?image_url ?koordinat ?nama
+    HAVING (BOUND(?nama))
+    """
+
+    sparql.setQuery(allQuery)
+    sparql.setReturnFormat(JSON)
+    all = sparql.queryAndConvert()
+    all_hospital = all["results"]["bindings"]
+    return all_hospital
+
+def get_closest_hospitals(user_latitude, user_longitude):
+    all_hospital = get_all_hospitals()
+    coordinates_dict = {}
+    for data in all_hospital:
+        if data.get('koordinat') and data['koordinat'].get('value'):
+            koordinat = data['koordinat']['value']
+            koordinat = koordinat.split(',')
+            latitude = koordinat[0]
+            longitude = koordinat[1]
+            
+            if data.get('nama') and data['nama'].get('value'):
+                name = data['nama']['value']
+                coordinates_dict[name] = [latitude, longitude]
+
+    distances_dict = {}
+    for name, coords in coordinates_dict.items():
+        lat2, lon2 = map(float, coords)
+        distance = haversine_distance(user_latitude, user_longitude, lat2, lon2)
+        distances_dict[name] = distance
+
+    sorted_distances = sorted(distances_dict.items(), key=itemgetter(1))[:4]
+    closest_hospitals_details = []
+    for name, distance in sorted_distances:
+        for data in all_hospital:
+            if data.get('nama') and data['nama']['value'] == name:
+                hospital_details = {
+                    'data': data,
+                    'distance': distance
+                }
+                closest_hospitals_details.append(hospital_details)
+                break 
+
+        if len(closest_hospitals_details) == 4:
+            break
+
+    return closest_hospitals_details
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
   
